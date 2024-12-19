447e4d743c8443051e5aa76a06a9eb133c0cc844
vladg <gudzovskijv539@gmail.com>
2024-12-19 16:41:15 +0200

    add file

--- components/structures.py
+++ components/structures.py
@@ -3,12 +3,10 @@ from typing import Optional
 from pydantic import HttpUrl, BaseModel
 
 
-# input parameters
 class UrlInput(BaseModel):
     url: HttpUrl
 
 
-# output parameters
 class ResponseParsing(BaseModel):
     title: str
     urls: list[Optional[str]]
